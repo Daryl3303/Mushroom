87a1d7797c668d0a28d61dce65c4c08585e2343b
John Daryl Abkilan <abkilanjohndaryl03@gmail.com>
2025-05-15 08:12:07 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Dashboard from './components/dashboard'
+function App() {
+  
+
+  return (
+  <div className="w-full h-[100vh]">
+    <Dashboard/>
+  </div>
+      
+  )
+}
+
+export default App
